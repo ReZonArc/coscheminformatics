af4f86d45f94a530261afbec6aaa76f513bf0ea3
Linas Vepstas <linasvepstas@gmail.com>
2022-07-17 13:31:34 -0500

    A more realistic example

--- examples/reaction.scm
+++ examples/reaction.scm
@@ -9,12 +9,13 @@
 (use-modules (opencog) (opencog cheminformatics))
 (use-modules (opencog exec))
 
-; This defines an esterification reaction. It is is simple, but
-; incomplete, as will become apparent shortly. Patience!
+; This defines an esterification reaction.
 (define esterification
 	(BindLink
 		; Variable declaration
 		(VariableList
+
+			; Typed variables, specifying specific atoms.
 			(TypedVariable (Variable "$varH1") (Type 'H))
 			(TypedVariable (Variable "$varH2") (Type 'H))
 			(TypedVariable (Variable "$varC1") (Type 'C))
@@ -22,6 +23,12 @@
 			(TypedVariable (Variable "$varO1") (Type 'O))
 			(TypedVariable (Variable "$varO2") (Type 'O))
 			(TypedVariable (Variable "$varO3") (Type 'O))
+
+			; Untyped variables that will match to anything
+			(Variable "carboxy moiety")
+			(Variable "hydroxy moiety")
+			(Glob "rest of carboxy")
+			(Glob "rest of hydroxy")
 		)
 		; Premise: Functional groups found in some educts
 		(AndLink
@@ -30,11 +37,15 @@
 				(DB (Variable "$varC1") (Variable "$varO1"))
 				(SB (Variable "$varC1") (Variable "$varO2"))
 				(SB (Variable "$varO2") (Variable "$varH1"))
+				(SB (Variable "$varC1") (Variable "carboxy moiety"))
+				(Glob "rest of carboxy")
 			)
 			; Look for hydroxyl group
 			(Molecule
 				(SB (Variable "$varC2") (Variable "$varO3"))
 				(SB (Variable "$varO3") (Variable "$varH2"))
+				(SB (Variable "$varC2") (Variable "hydroxy moiety"))
+				(Glob "rest of hydroxy")
 			)
 		)
 		; Clause: Formation of products
@@ -44,6 +55,12 @@
 				(DB (Variable "$varC1") (Variable "$varO1"))
 				(SB (Variable "$varC1") (Variable "$varO2"))
 				(SB (Variable "$varO2") (Variable "$varC2"))
+
+				(SB (Variable "$varC1") (Variable "carboxy moiety"))
+				(Glob "rest of carboxy")
+
+				(SB (Variable "$varC2") (Variable "hydroxy moiety"))
+				(Glob "rest of hydroxy")
 			)
 			; Produce water
 			(Molecule
@@ -62,19 +79,23 @@
 	(DB (C "some carb") (O "oxy one"))
 	(SB (C "some carb") (O "oxy two"))
 	(SB (O "oxy two") (H "carboxyl proton"))
+	; Some nonsense moiety, for pattern matching only.
+	(SB (C "some carb") (Fe "Bushehr"))
+	(SB (Fe "Bushehr") (Ni "phase II"))
 )
 
 ; A hydroxyl group
 (Molecule
 	(SB (C "hydroxyl carbon") (O "hydroxyl oxy"))
 	(SB (O "hydroxyl oxy") (H "hydroxyl proton"))
+	; Another nonsense moiety, for pattern matching
+	(SB (O "hydroxyl oxy") (Zn "Iranian"))
+	(SB (Zn "Iranian") (Cu "Uranium"))
 )
 
 ; Perform the reaction
 (cog-execute! esterification)
 
 ; ------------------------------------------------
-; What's wrong with this example? Two things: The search pattern failed
-; to specify the "R" remaining part of the molecules. And the demo
-; molecules also don't have the "R" part. Lets fix this ...
-
+; The end.
+; That's all, folks!
