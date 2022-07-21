bb30979ce819a2c2fe55fa21870e7e8c0f70af13
Linas Vepstas <linasvepstas@gmail.com>
2022-07-21 18:45:13 -0500

    Rename variables in the demo, to better understand them.

--- examples/reaction.scm
+++ examples/reaction.scm
@@ -21,12 +21,13 @@
 		(VariableList
 
 			; Typed variables, specifying specific atoms.
-			(TypedVariable (Variable "$varH1") (Type 'H))
-			(TypedVariable (Variable "$varH2") (Type 'H))
-			(TypedVariable (Variable "$varC1") (Type 'C))
-			(TypedVariable (Variable "$varO1") (Type 'O))
-			(TypedVariable (Variable "$varO2") (Type 'O))
-			(TypedVariable (Variable "$varO3") (Type 'O))
+			(TypedVariable (Variable "$carboxyH1") (Type 'H))
+			(TypedVariable (Variable "$carboxyO1") (Type 'O))
+			(TypedVariable (Variable "$carboxyC1") (Type 'C))
+			(TypedVariable (Variable "$carboxyO2") (Type 'O))
+
+			(TypedVariable (Variable "$hydroxH1") (Type 'H))
+			(TypedVariable (Variable "$hydroxO1") (Type 'O))
 
 			; Untyped variables that will match to anything
 			(Variable "carboxy moiety")
@@ -38,10 +39,10 @@
 		(AndLink
 			; Look for carboxyl group
 			(Molecule
-				(DB (Variable "$varC1") (Variable "$varO1"))
-				(SB (Variable "$varC1") (Variable "$varO2"))
-				(SB (Variable "$varO2") (Variable "$varH1"))
-				(SB (Variable "$varC1") (Variable "carboxy moiety"))
+				(DB (Variable "$carboxyC1") (Variable "$carboxyO2"))
+				(SB (Variable "$carboxyC1") (Variable "$carboxyO1"))
+				(SB (Variable "$carboxyO1") (Variable "$carboxyH1"))
+				(SB (Variable "$carboxyC1") (Variable "carboxy moiety"))
 
 				; Globs match one or more bonds.  To match zero,
 				; change the lower bound by declaring it lik this:
@@ -50,15 +51,15 @@
 				(Glob "rest of carboxy")
 			)
 
-			; The above will happily match `$varO2` and `carboxy moiety`
+			; The above will happily match `$carboxyO1` and `carboxy moiety`
 			; to the same atom. But we don't want that, so demand that
 			; they be distinct.
-			(Not (Identical (Variable "$varO2") (Variable "carboxy moiety")))
+			(Not (Identical (Variable "$carboxyO1") (Variable "carboxy moiety")))
 
 			; Look for hydroxyl group
 			(Molecule
-				(SB (Variable "$varO3") (Variable "$varH2"))
-				(SB (Variable "$varO3") (Variable "hydroxy moiety"))
+				(SB (Variable "$hydroxO1") (Variable "$hydroxH1"))
+				(SB (Variable "$hydroxO1") (Variable "hydroxy moiety"))
 				(Glob "rest of hydroxy")
 			)
 		)
@@ -66,19 +67,19 @@
 		(AndLink
 			; Produce ester
 			(Molecule
-				(DB (Variable "$varC1") (Variable "$varO1"))
-				(SB (Variable "$varC1") (Variable "$varO2"))
+				(DB (Variable "$carboxyC1") (Variable "$carboxyO2"))
+				(SB (Variable "$carboxyC1") (Variable "$carboxyO1"))
 
-				(SB (Variable "$varC1") (Variable "carboxy moiety"))
+				(SB (Variable "$carboxyC1") (Variable "carboxy moiety"))
 				(Glob "rest of carboxy")
 
-				(SB (Variable "$varO2") (Variable "hydroxy moiety"))
+				(SB (Variable "$carboxyO1") (Variable "hydroxy moiety"))
 				(Glob "rest of hydroxy")
 			)
 			; Produce water
 			(Molecule
-				(SB (Variable "$varO3") (Variable "$varH1"))
-				(SB (Variable "$varO3") (Variable "$varH2"))
+				(SB (Variable "$hydroxO1") (Variable "$carboxyH1"))
+				(SB (Variable "$hydroxO1") (Variable "$hydroxH1"))
 			)
 		)
 	)
@@ -89,21 +90,22 @@
 ;
 ; Carboxyl group
 (Molecule
-	(DB (C "some carb") (O "oxy one"))
-	(SB (C "some carb") (O "oxy two"))
+	(DB (C "the carboxyl carb") (O "oxy one"))
+	(SB (C "the carboxyl carb") (O "oxy two"))
 	(SB (O "oxy two") (H "carboxyl proton"))
-	(SB (C "some carb") (Fe "Bushehr"))
+	(SB (C "the carboxyl carb") (Fe "carbox R"))
 	; Some nonsense moiety, for pattern matching only.
-	(SB (Fe "Bushehr") (Ni "phase II"))
+	(SB (Fe "carbox R") (Ni "more carbox junk"))
 )
 
 ; A hydroxyl group
 (Molecule
 	(SB (O "hydroxyl oxy") (H "hydroxyl proton"))
+
 	; Another nonsense moiety, for pattern matching
 	(SB (C "hydroxyl carbon") (O "hydroxyl oxy"))
-	(SB (C "hydroxyl carbon") (Zn "Iranian"))
-	(SB (Zn "Iranian") (Cu "Uranium"))
+	(SB (C "hydroxyl carbon") (Zn "hydrox R"))
+	(SB (Zn "hydrox R") (Cu "junk hydrox moiety"))
 )
 
 ; Perform the reaction
